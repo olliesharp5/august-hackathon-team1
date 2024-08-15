f8d194495947f09327f1b038dc87bd136941da08
Mihaly Lovrencsics <lomysa@gmail.com>
2024-08-15 20:04:23 +0000

    Add global variables, playGame, startLevel functions

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,7 +1,6 @@
 import { saveScore, getLeaderboard, testFirestoreConnection } from '/assets/javascript/firebase.js';
 
 // firebase
-
 document.addEventListener('DOMContentLoaded', async () => {
     try {
       const testDocId = await testFirestoreConnection();
@@ -11,7 +10,16 @@ document.addEventListener('DOMContentLoaded', async () => {
     }
   });
 
+
+// gameState variables
+let level = 0;
+let remainingDucks = 0;
+let misses = 0;
+let maxMisses = 3;
+let score = 0;
+
 // Menu
+// Calls playGame() or leaderBoard()
 document.addEventListener("DOMContentLoaded", function () {
     let buttons = document.getElementsByTagName("button");
     for (let button of buttons) {
@@ -27,10 +35,41 @@ document.addEventListener("DOMContentLoaded", function () {
 }
 });
 
+/**
+ * This function does:
+ * Set the level variable
+ * Modify the DOM
+ * call the startLevel function
+ **/
 function playGame() {
-    console.log("you have clicked playgame");
+    // swap the body content to canvas
+    let gameContent = `
+    <main>
+    <canvas id="game-area">
+    </canvas>
+    </main>
+    `;
+    document.body.innerHTML = gameContent;
+    startLevel();
 }
 
+/**
+ * This function does:
+ * handle the leaderboard
+ **/
 function leaderBoard() {
     console.log("you have clicked leaderboard");
 }
+
+/**
+ * This function does:
+ * start the first level
+ * set the variables
+ **/
+function startLevel() {
+    level = 1;
+    remainingDucks = 3;
+    misses = 0;
+    maxMisses = 3;
+    score = 0;
+};
