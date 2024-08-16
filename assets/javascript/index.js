8e7a6185dfd27340f2859b9a4befaf0e277ad9e4
Jaimie <jaimie.j.hemmings@gmail.com>
2024-08-16 12:59:04 +0100

    add: volume mute functionality

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -96,6 +96,27 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   }
 
+  // Set volume to 50% on load
+  music.volume = 0.5;
+
+  // Toggle volume mute
+  const volumeToggle = document.getElementById("volumeToggle");
+  const muteIcon = `<svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+    <path d="M5.707 4.293a1 1 0 0 0-1.414 1.414l14 14a1 1 0 0 0 1.414-1.414l-.004-.005C21.57 16.498 22 13.938 22 12a9.972 9.972 0 0 0-2.929-7.071 1 1 0 1 0-1.414 1.414A7.972 7.972 0 0 1 20 12c0 1.752-.403 3.636-1.712 4.873l-1.433-1.433C17.616 14.37 18 13.107 18 12c0-1.678-.69-3.197-1.8-4.285a1 1 0 1 0-1.4 1.428A3.985 3.985 0 0 1 16 12c0 .606-.195 1.335-.59 1.996L13 11.586V6.135c0-1.696-1.978-2.622-3.28-1.536L7.698 6.284l-1.99-1.991ZM4 8h.586L13 16.414v1.451c0 1.696-1.978 2.622-3.28 1.536L5.638 16H4a2 2 0 0 1-2-2v-4a2 2 0 0 1 2-2Z"/>
+  </svg>`;
+  const notMutedIcon = `<svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.5 8.43A4.985 4.985 0 0 1 17 12a4.984 4.984 0 0 1-1.43 3.5m2.794 2.864A8.972 8.972 0 0 0 21 12a8.972 8.972 0 0 0-2.636-6.364M12 6.135v11.73a1 1 0 0 1-1.64.768L6 15H4a1 1 0 0 1-1-1v-4a1 1 0 0 1 1-1h2l4.36-3.633a1 1 0 0 1 1.64.768Z"/>
+  </svg>`;
+
+  volumeToggle.addEventListener("click", function () {
+    if (music.muted) {
+      music.muted = false;
+      volumeToggle.innerHTML = notMutedIcon;
+    } else {
+      music.muted = true;
+      volumeToggle.innerHTML = muteIcon;
+    }
+  });
 });
 
 // Menu
